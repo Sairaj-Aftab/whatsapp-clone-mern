f655db414cbb19c70c4a1860e57e9b7b053c7405
Sairaj Aftab <95501240+Sairaj-Aftab@users.noreply.github.com>
2024-03-22 21:47:57 +0600

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "*",
     credentials: true,
   })
 );
